abbe9ccbfaeb547389723859eb3c041774ac992c
VincoRegrut <88244255+VincoRegrut@users.noreply.github.com>
2021-07-31 17:35:02 +0200

    Add files via upload

---
+++ src/main/java/com/example/first/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void lblLuckaClick( View v ) {
+        ((TextView)findViewById( R.id.lblLucka )).setText( R.string.lblLucka_text );
+        Toast.makeText( this, "The label was changed", Toast.LENGTH_LONG ).show();
+    }
+}
